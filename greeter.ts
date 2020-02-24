848ab0a96823ae6b252d7feeb5de626e9f542f97
Tguaetta <60271078+Tguaetta@users.noreply.github.com>
2020-02-24 16:54:13 -0500

    Final Commit

--- greeter.ts
+++ greeter.ts
@@ -1,1 +1,9 @@
-// your name - 2020 
+// Thomas Guaetta - 2020
+
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+
+document.body.textContent = greeter(user);
